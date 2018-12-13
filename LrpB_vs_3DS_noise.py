9609fc0a3038673b7ba04fb762be67bd9125794c
sophiedeb <sdebuyl@ulb.ac.be>
2018-12-13 17:10:12 +0100

    add histogram for fano factors

--- LrpB_vs_3DS_noise.py
+++ LrpB_vs_3DS_noise.py
@@ -37,7 +37,7 @@ mynonmonotonicity= 'monotonic', 'non-monotonic 1', 'non-monotonic 2', 'non-monot
 
 
 #load LrpB 3DS data
-file = '/Users/sdebuyl/sgo-py/stoch_LrpB/'  # '/Users/sdebuyl/sgo/stoch_' + mymodel + 'DS_LrpB/'
+file = '/Users/sdebuyl/stoch_LrpB/'  # '/Users/sdebuyl/sgo/stoch_' + mymodel + 'DS_LrpB/'
 
 LrpB_shift = 0  # this variable was introduced to be able to change label the position of numbers keeping track of different simulations
 namefiletosavedata = 'stoch_' + mymodel + 'DS'
@@ -130,7 +130,7 @@ print('fraction spiky sol',counter_spikes_LrpB,tot_simulations_LrpB,counter_spik
 
 
 #load LrpB 3DS data
-file = '/Users/sdebuyl/sgo-py/stoch_' + mymodel + 'DS/'
+file = '/Users/sdebuyl/stoch_' + mymodel + 'DS/'
 
 LrpB_shift = 0  # this variable was introduced to be able to change label the position of numbers keeping track of different simulations
 namefiletosavedata = 'stoch_' + mymodel + 'DS'
@@ -309,11 +309,97 @@ for k in range(len(lists[jj])):  #np.random.choice(len(lists[jj]),160)
 
 
 
+##############
+##############
+##############
 
 
 
 
+#### extract data MDS
 
+mymodel= 'M'
+vartoplot=-1
+oscillators = MDS
+
+
+file = '/Users/sdebuyl/stoch_MDS/'  # '/Users/sdebuyl/sgo/stoch_' + mymodel + 'DS_LrpB/'
+
+LrpB_shift = 0  # this variable was introduced to be able to change label the position of numbers keeping track of different simulations
+namefiletosavedata = 'stoch_' + mymodel + 'DS'
+# actually import the data:
+mylist = ospack.listdir(file)
+# create list for each type of non-monotonicity
+list0 = list()
+list1 = list()
+list2 = list()
+list3 = list()
+# create a counter for spiky solution
+counter_spikes_LrpB=0
+for kk in mylist:
+
+    if kk.find('stoch_' + mymodel + 'DS_parms_') != -1:
+        end = kk.find('.txt')
+        #print('looking', kk[16 + LrpB_shift:17 + LrpB_shift], int(kk[18 + LrpB_shift:end]))
+        if kk[16 + LrpB_shift:17 + LrpB_shift] == '0':
+            list0.append(int(kk[18 + LrpB_shift:end]))
+        if kk[16 + LrpB_shift:17 + LrpB_shift] == '1':
+            list1.append(int(kk[18 + LrpB_shift:end]))
+        if kk[16 + LrpB_shift:17 + LrpB_shift] == '2':
+            list2.append(int(kk[18 + LrpB_shift:end]))
+        if kk[16 + LrpB_shift:17 + LrpB_shift] == '3':
+            list3.append(int(kk[18 + LrpB_shift:end]))
+
+lists = [list0, list1, list2, list3]
+
+print('len list 0', len(list0))
+print('len list 1', len(list1))
+print('len list 2', len(list2))
+print('len list 3', len(list3))
+tot_simulations_LrpB=len(list0)+len(list1)+len(list2)+len(list3)
+
+# we will plot the fano factor as a function of the mean value of the dimer concentration
+# we first create empty arrays to be filled with means and variances of each simulation
+mymeans0 = np.zeros(len(list0))
+mymeans1 = np.zeros(len(list1))
+mymeans2 = np.zeros(len(list2))
+mymeans3 = np.zeros(len(list3))
+
+mymeans_wt = [mymeans0, mymeans1, mymeans2, mymeans3]
+
+myvars0 = np.zeros(len(list0))
+myvars1 = np.zeros(len(list1))
+myvars2 = np.zeros(len(list2))
+myvars3 = np.zeros(len(list3))
+
+myvars_wt = [myvars0, myvars1, myvars2, myvars3]
+
+
+
+myfanofactor4=np.array([])
+
+jj=1
+
+for k in range(len(lists[jj])):  #np.random.choice(len(lists[jj]),160)
+    mymeans_wt = np.loadtxt(file + namefiletosavedata + '_means_' + str(jj) + '_' + str(lists[jj][k]) + '.txt')
+    myvars_wt = np.loadtxt(file + namefiletosavedata + '_vars_' + str(jj) + '_' + str(lists[jj][k]) + '.txt')
+    temp = mymeans_wt[vartoplot]
+    if temp!=0:
+        myfanofactor4=np.append(myfanofactor4,myvars_wt[vartoplot] / mymeans_wt[vartoplot])
+
+
+
+############## end extract MDS info
+##############
+##############
+
+
+
+
+sorted_data4 = np.sort(myfanofactor4)
+y4=np.arange(sorted_data4.size)
+y4=y4.astype(float)
+y4=y4/np.max(y4)
 
 sorted_data3 = np.sort(myfanofactor3)
 y3=np.arange(sorted_data3.size)
@@ -336,7 +422,23 @@ fig = plt.figure()
 plt.plot(sorted_data, y1/np.max(y1) ,label='LrpB')
 plt.plot(sorted_data2,y2/np.max(y2) ,label='3DS')
 plt.plot(sorted_data3,y3/np.max(y3),label='2DS')
+plt.plot(sorted_data4,y4/np.max(y4),label='MDS')
 plt.xscale('log')
 #plt.xlim([0.1,10])
 plt.legend()
 plt.savefig('fano_factor_large_reduced.pdf')
+
+
+
+fig = plt.figure()
+plt.hist(myfanofactor4, bins='auto',alpha=.3,label='MDS')
+plt.hist(myfanofactor3, bins='auto',alpha=.3,label='2DS')
+plt.hist(myfanofactor2, bins='auto',alpha=.3,label='3DS')
+plt.hist(myfanofactor, bins='auto',alpha=.3,label='LrpB')
+plt.legend(loc='upper right')
+plt.savefig('hist.pdf')
+
+np.savetxt('fanoLrpB.txt',myfanofactor)
+np.savetxt('fano3DS.txt',myfanofactor2)
+np.savetxt('fano2DS.txt',myfanofactor3)
+np.savetxt('fanoMDS.txt',myfanofactor4)
